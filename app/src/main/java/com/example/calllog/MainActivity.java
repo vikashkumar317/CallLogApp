4b4d3f49af43bf459d5f9198a5f1c1a58d7c4c28
Vikash <mrvikash31j@gmail.com>
2020-08-11 20:44:19 +0530

    Issue Fixed

--- app/src/main/java/com/example/calllog/MainActivity.java
+++ app/src/main/java/com/example/calllog/MainActivity.java
@@ -17,21 +17,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] strNumber = {"9709825732"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED){
-//            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CALL_LOG)){
-//
-//            }
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, 1);
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CALL_LOG)){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, 1);
+            }
+            else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, 1);
+            }
         }
         else{
             TextView textView = findViewById(R.id.callTV);
-            textView.setText(getCallDetails());
+            textView.setText(getCallDetails(strNumber));
         }
     }
 
@@ -41,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
                 TextView textView = findViewById(R.id.callTV);
-                textView.setText(getCallDetails());
+                textView.setText(getCallDetails(strNumber));
             }
             else{
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
@@ -49,37 +51,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String getCallDetails(){
+    private String getCallDetails(String[] strNumber){
         StringBuffer callDetails = new StringBuffer();
-        Cursor managedCursor = getContentResolver().query(CallLog.Calls.CONTENT_URI,null,null,null,null);
+        Cursor managedCursor = getContentResolver().query(CallLog.Calls.CONTENT_URI,null, CallLog.Calls.NUMBER + " = ? ", strNumber , CallLog.Calls.DATE + " DESC");
         int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
         int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
         int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
         int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
         callDetails.append("Call Details:\n");
-        while(managedCursor.moveToNext()){
-            String phNumber = managedCursor.getString(number);
-            String callType = managedCursor.getString(type);
-            String callDate = managedCursor.getString(date);
-            Date callDayTime = new Date(Long.valueOf(callDate));
-            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm");
-            String dateString = formatter.format(callDayTime);
-            String callDuration = managedCursor.getString(duration);
-            String dir = null;
-            int dircode = Integer.parseInt(callType);
-            switch (dircode){
-                case CallLog.Calls.OUTGOING_TYPE:
-                    dir = "OUTGOING CALL";
-                    break;
-                case CallLog.Calls.INCOMING_TYPE:
-                    dir = "INCOMING CALL";
-                    break;
-                case CallLog.Calls.MISSED_TYPE:
-                    dir = "MISSED CALL";
-                    break;
+        if(managedCursor.moveToFirst()) {
+            while (managedCursor.moveToNext()) {
+                String phNumber = managedCursor.getString(number);
+                String callType = managedCursor.getString(type);
+                String callDate = managedCursor.getString(date);
+                Date callDayTime = new Date(Long.valueOf(callDate));
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm");
+                String dateString = formatter.format(callDayTime);
+                String callDuration = managedCursor.getString(duration);
+                String dir = null;
+                int dircode = Integer.parseInt(callType);
+                switch (dircode) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        dir = "OUTGOING CALL";
+                        break;
+                    case CallLog.Calls.INCOMING_TYPE:
+                        dir = "INCOMING CALL";
+                        break;
+                    case CallLog.Calls.MISSED_TYPE:
+                        dir = "MISSED CALL";
+                        break;
+                }
+                callDetails.append("\nPhone Number: " + phNumber + "\nCallType: " + dir + "\nCall Date: " + dateString + "\nCall Duration: " + callDuration + " s");
+                callDetails.append("\n---------------------------------");
             }
-            callDetails.append("\nPhone Number: "+ phNumber +"\nCallType: "+ dir +"\nCall Date: "+ dateString + "\nCall Duration: "+ callDuration+" s");
-            callDetails.append("\n---------------------------------");
         }
         managedCursor.close();
         return callDetails.toString();
